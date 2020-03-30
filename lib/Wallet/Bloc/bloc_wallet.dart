a34936b5b9aeca6fed73c2812aeb9fa2b2f3d955
angel <angel@alphacode.com.br-at-503688510464>
2020-03-29 22:03:42 -0300

    detect exist wallet

--- lib/Wallet/Bloc/bloc_wallet.dart
+++ lib/Wallet/Bloc/bloc_wallet.dart
@@ -1,16 +1,21 @@
-import 'dart:async';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:async';
 // import 'package:bitcoin_flutter/bitcoin_flutter.dart';
 import 'package:bip39/bip39.dart' as bip39;
+import 'package:wallet_app/Wallet/Models/wallets_model.dart';
 
 class BlocWallet implements Bloc {
-
-  final _blocController = StreamController<int>();
-
-  Stream<int> get minhaStream => _blocController.stream;
+  final _walletModel = WalletsModel();
+  final _existeWallet = StreamController<bool>();
+  Stream<bool> get exist => _existeWallet.stream;
+  void existWallet() {
+    var data = _walletModel.getObject(1);
+    print(data);
+    _existeWallet.sink.add(true);
+  }
 
   static String mnemonic() {
-
     return bip39.generateMnemonic();
   }
 
