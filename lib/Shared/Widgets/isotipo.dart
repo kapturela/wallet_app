0c0ee449ea2aff7c532e33ed4a7f8f6a315fb3ec
angel <angel@alphacode.com.br-at-503688510464>
2020-04-05 07:55:33 -0300

    isotipo

---
+++ lib/Shared/Widgets/isotipo.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Isotipo extends StatelessWidget {
+
+  final double height;
+  final double width;
+  final bool gray;
+
+  Isotipo({Key key, @required this.height, @required this.width, this.gray});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      height: height,
+      width: width,
+      margin: EdgeInsets.only(bottom: 24),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: _getLogo(),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  AssetImage _getLogo() {
+    if(gray) {
+      return AssetImage("assets/images/ISOTIPO_GRIS.png");
+    } else {
+      return AssetImage("assets/images/ISOTIPO.png");
+    }
+
+  }
+
+}
